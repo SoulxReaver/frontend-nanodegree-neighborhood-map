7c0c6d8db810045bccc18adbdfc631e630c03321
soulxreaver <alextong0628@gmail.com>
2017-05-01 22:48:42 -0700

    fixed filter bug

--- src/js/main.js
+++ src/js/main.js
@@ -3,73 +3,148 @@ var locations = [
 		name: 'Seattle Public Library-Central Library',
 		lat: 47.6067006,
 		lng: -122.3325009,
-        visible: true
+        visible: true,
+        index: 0
 	},
 	{
 		name: 'Tacoma Done',
 		lat: 47.2366694,
 		lng: -122.4270299,
-        visible: true
+        visible: true,
+        index: 1
 	},
 	{
 		name: 'Microsoft Corporation',
 		lat: 47.5556769,
 		lng: -122.0495224,
-        visible: true
+        visible: true,
+        index: 2
 	},
 	{
 		name: 'Mount Rainier National Park',
 		lat: 46.8799663,
 		lng: -121.7269094,
-        visible: true
+        visible: true,
+        index: 3
 	},
 	{
 		name: 'Seattle Premium Outlets',
 		lat: 48.093531,
 		lng: -122.188311,
-        visible: true
+        visible: true,
+        index: 4
 	}
 
 ];
-
+var clientId = 'KX5QOHCVYDOBKYCFM2FT3U5GXUOPKEAW20UUNBBTXYF11H44';
+var clientSecret = 'U52N3NALZIHQSKJVA3NFXZTTUKZLCHA0JAUW5KNSE2OSAD5X';
 var locMarker = [];
 var searchStreet;
 var map;
 var filteredLocations = [];
 
+function initMap() {
+    ko.applyBindings(new AppViewModel());
+    var uluru = {lat: 47.8104922, lng: -122.248259}; //seattle
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: uluru
+    });
+    
+    filterList();
+    setupLocMarker();
+}
+
+function AppViewModel() {
+    searchStreet = ko.observable("");
+    displayLocations = ko.observableArray();
+}
+
 function setupLocMarker() {
     locMarker = [];
-    locations.forEach(function (loc) {
-        if (loc) {
-            var marker = new google.maps.Marker({
-                position: loc,
-                map: map,
-                title: loc.name
-            });
+    for(var i = 0; i <locations.length; i++) {
+        var loc = locations[i];
+        var marker = new google.maps.Marker({
+            position: loc,
+            map: map,
+            title: loc.name
+        });
+        setupInfoWindow(loc, marker, locMarker, i);
+    }
+}
 
-            var contentString = 
-                '<div>' +
-                    '<h3>' + loc.name + '</h3>' +
-                    '<p>latitude: ' + loc.lat + '</p>' +
-                    '<p>longitude: ' + loc.lng + '</p>' +
-                '</div>';
+function setupInfoWindow(loc, marker, index) {
+    var foursquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=' + 
+    clientId +
+    '&client_secret=' + clientSecret +
+    '&v=20130815&ll=' + loc.lat + ',' + loc.lng + '&query=' + loc.name;
+    var foursquareResult = {};
+    $.getJSON(foursquareUrl).done(function(data) {
+        var results = data.response.venues[0];
+        foursquareResult.url = results.url;
+        foursquareResult.street = results.location.formattedAddress[0];
+        foursquareResult.city = results.location.formattedAddress[1];
+        foursquareResult.phone = results.contact.phone;
+        var contentString = 
+        '<div>' +
+            '<h3>' + loc.name + '</h3>' +
+            '<div>url: ' + foursquareResult.url + '</div>' +
+            '<div>street: ' + foursquareResult.street + '</div>' +
+            '<div>city: ' + foursquareResult.city + '</div>' +
+            '<div>phone: ' + foursquareResult.phone + '</div>' +
+            '<div>latitude: ' + loc.lat + '</div>' +
+            '<div>longitude: ' + loc.lng + '</div>' +
+        '</div>';
 
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
 
-            marker.infowindow = infowindow;
+        marker.infowindow = infowindow;
 
-            marker.addListener('click', toggleBounce);
-            locMarker.push(marker);
+        marker.addListener('click', toggleBounce);
+        
+        marker.setMap(map);
+        loc.marker = marker;
+        filteredLocations[loc.index] = loc;
+        locMarker[loc.index] = marker;
+        displayLocations.push(loc);
+    }).fail(function() {
+        alert("There was an error with the Foursquare API call. Please Try again later.");
+    });
+}
+
+function filterList() {
+    ko.computed( function() {
+		var filter = searchStreet().toLowerCase();
+		if (!filter) {
+            for (var i = 0; i < locations.length; i++) {
+                locations[i].visible = true;
+            }
+		} else {
+			ko.utils.arrayFilter(locations, function(locationItem) {
+				var string = locationItem.name.toLowerCase();
+				var result = (string.search(filter) >= 0);
+				locationItem.visible = result;
+			});
+		}
+        hideAndShowMarker();
+	}, this);
+}
+
+function hideAndShowMarker() {
+    displayLocations.removeAll();
+    for (var i = 0; i < locMarker.length; i++) {
+        if(locations[i].visible) {
+            locMarker[i].setMap(map);
         }
         else {
-            loc.setMap(null);
+            locMarker[i].setMap(null);
         }
-    });
-
-
+        displayLocations.push(locations[i]);
+    }
 }
+
 function locationsListClicked (place) {
     toggleBounce.call(place.marker);
 }
@@ -83,66 +158,12 @@ function toggleBounce() {
     }
 }
 
-function hideAndShowMarker() {    
-    
-    filteredLocations.removeAll();
-    for (var i = 0; i < locMarker.length; i++) {
-        if(locations[i].visible) {
-            locMarker[i].setMap(map);
-            locations[i].marker = locMarker[i];
-
-            filteredLocations.push(locations[i]);
-        }
-        else {
-            locMarker[i].setMap(null);
-        }
-    }
-}
-
-function filterList() {
-    ko.computed( function() {
-		var filter = searchStreet().toLowerCase();
-		if (!filter) {
-			locations.forEach(function(locationItem){
-				locationItem.visible = true;
-			});
-		} else {
-			ko.utils.arrayFilter(locations, function(locationItem) {
-				var string = locationItem.name.toLowerCase();
-				var result = (string.search(filter) >= 0);
-				locationItem.visible = result;
-			});
-		}
-        hideAndShowMarker();
-	}, this);
-}
-
-function initMap() {
-    ko.applyBindings(new AppViewModel());
-
-    var uluru = {lat: 47.8104922, lng: -122.248259}; //seattle
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: uluru
-    });
-    
-    setupLocMarker();
-    filterList();
-}
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-}
-
-function AppViewModel() {
-    
-    searchStreet = ko.observable("");
-    filteredLocations = ko.observableArray(filteredLocations);
-
 }
 
 function errorHandling() {
